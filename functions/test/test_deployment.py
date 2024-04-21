2cf2a172f53b48b1807cd04adfbb8051da576199
Michinobu Maeda <michinobumaeda@gmail.com>
2024-04-21 23:39:29 +0900

    deployent test #2

--- functions/test/test_deployment.py
+++ functions/test/test_deployment.py
@@ -37,60 +37,60 @@ class TestMain(unittest.TestCase):
             }
         )
 
-    @mock.patch("requests.get", side_effect=mocked_requests_get_success)
-    def test_set_ui_version_update(
-        self,
-        requests_get_mock,
-    ):
-        # Prepare
-        ver = "1.2.3"
-        event = MockEvent(
-            "test_success",
-            MockDocSnap("test/path", {"uiVersion": "0.0.0"}),
-        )
-        event.data.reference.update = MagicMock(return_value=None)
+    # @mock.patch("requests.get", side_effect=mocked_requests_get_success)
+    # def test_set_ui_version_update(
+    #     self,
+    #     requests_get_mock,
+    # ):
+    #     # Prepare
+    #     ver = "1.2.3"
+    #     event = MockEvent(
+    #         "test_success",
+    #         MockDocSnap("test/path", {"uiVersion": "0.0.0"}),
+    #     )
+    #     event.data.reference.update = MagicMock(return_value=None)
 
-        # Run
-        set_ui_version(event)
+    #     # Run
+    #     set_ui_version(event)
 
-        # Check
-        event.data.reference.update.assert_called_with(
-            {
-                "uiVersion": ver,
-                "updatedAt": firestore.SERVER_TIMESTAMP,
-            }
-        )
+    #     # Check
+    #     event.data.reference.update.assert_called_with(
+    #         {
+    #             "uiVersion": ver,
+    #             "updatedAt": firestore.SERVER_TIMESTAMP,
+    #         }
+    #     )
 
-    @mock.patch("requests.get", side_effect=mocked_requests_get_success)
-    def test_set_ui_version_no_action(
-        self,
-        requests_get_mock,
-    ):
-        # Prepare
-        ver = "1.2.3"
-        event = MockEvent(
-            "test_success",
-            MockDocSnap("test/path", {"uiVersion": ver}),
-        )
-        event.data.reference.update = MagicMock(return_value=None)
+    # @mock.patch("requests.get", side_effect=mocked_requests_get_success)
+    # def test_set_ui_version_no_action(
+    #     self,
+    #     requests_get_mock,
+    # ):
+    #     # Prepare
+    #     ver = "1.2.3"
+    #     event = MockEvent(
+    #         "test_success",
+    #         MockDocSnap("test/path", {"uiVersion": ver}),
+    #     )
+    #     event.data.reference.update = MagicMock(return_value=None)
 
-        # Run
-        set_ui_version(event)
+    #     # Run
+    #     set_ui_version(event)
 
-        # Check
-        event.data.reference.update.assert_not_called()
+    #     # Check
+    #     event.data.reference.update.assert_not_called()
 
-    @mock.patch("requests.get", side_effect=mocked_requests_get_failed)
-    def test_set_ui_version_failed(
-        self,
-        requests_get_mock,
-    ):
-        # Prepare
-        event = MockEvent("test_failed", MockDocSnap("test/path", {}))
-        event.data.reference.update = MagicMock(return_value=None)
+    # @mock.patch("requests.get", side_effect=mocked_requests_get_failed)
+    # def test_set_ui_version_failed(
+    #     self,
+    #     requests_get_mock,
+    # ):
+    #     # Prepare
+    #     event = MockEvent("test_failed", MockDocSnap("test/path", {}))
+    #     event.data.reference.update = MagicMock(return_value=None)
 
-        # Run
-        set_ui_version(event)
+    #     # Run
+    #     set_ui_version(event)
 
-        # Check
-        event.data.reference.update.assert_not_called()
+    #     # Check
+    #     event.data.reference.update.assert_not_called()
